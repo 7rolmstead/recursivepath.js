cce76ba0bcc78af147f81f1ed130bf3119142c91
7rolmstead <7rolmstead@stpatrick.org>
2017-02-17 10:50:03 -0600

    finished

--- path.js
+++ path.js
@@ -12,17 +12,28 @@ function buildPath(p,n){
 return path;
 }
 function stroll(path,index){
-  if(index==path.length){
+  if(index==path.length-1){
       return 1;
   }
-  else if(index==path.length-1){
+  else if(index>=path.length){
     return 0;
   }
   else{
     stroll(stroll,index++);
   }
 }
+function rsj(path,index){
+  if(index==path.length-1){
+    return 1;
 
+  }
+  else if(index>=path.length){
+    return 0;
+  }
+  else{
+    return rsj(path,index++)+rsj(path,index+2)+rsj(path,index+5);
+  }
+}
 
 //Test code
 let testpath1 = buildPath(0.10,15);
